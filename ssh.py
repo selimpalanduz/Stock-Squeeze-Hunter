c5cab58643bce263b150b66d9d005d5d9ed7e1a7
Selim Palanduz <selimpalanduz13@gmail.com>
2026-02-24 00:43:24 +0300

    Data is cleared.

--- ssh.py
+++ ssh.py
@@ -7,4 +7,12 @@ df = ydatas.download(f"{ticker}.IS", period="6mo", auto_adjust=True, progress=Fa
 if df.empty:
     print(f"{ticker} için veri bulunamadı.")
 else:
+    #If MultiIndex exists,fix.
+    if isinstance(df.columns,pd.MultiIndex):
+        df.columns= df.columns.get_level_values(0)
+    #Get only needed columns defined below:
+    df =df[["Open","High","Low","Close","Volume"]]
+    #Clear NaNs
+    df.dropna(inplace=True)
+
     print(df.tail(10))
