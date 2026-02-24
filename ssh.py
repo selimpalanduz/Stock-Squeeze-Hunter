521f1f6dbf30f1e842af2c6d9af71ac84df2590f
Selim Palanduz <selimpalanduz13@gmail.com>
2026-02-24 19:17:28 +0300

    TTM squeeze is added

--- ssh.py
+++ ssh.py
@@ -20,28 +20,47 @@ else:
     df["ADX"]=ta.trend.ADXIndicator(df["High"],df["Low"],df["Close"],window=14).adx()
     #RSI
     df["RSI"]=ta.momentum.RSIIndicator(df["Close"],window=14).rsi()
+    
+    #EMA-10 Based Bollinger Band.
+    ema=df["Close"].ewm(span=10,adjust=False).mean()
+    std=df["Close"].rolling(window=20).std()
+    df["BB_Upper"]=ema +2 * std
+    df["BB_Lower"]=ema -2 * std
+
+    #ATR:Needed for Keltner
+    df["ATR"]=ta.volatility.AverageTrueRange(df["High"],df["Low"],df["Close"],window=14).average_true_range()
+
+    #Keltner Channel
+    kc_mid=df["Close"].ewm(span=20,adjust=False).mean()
+    df["KC_Upper"]=kc_mid +1.5 *df["ATR"]
+    df["KC_Lower"]=kc_mid -1.5 *df["ATR"]
+    df.dropna(inplace=True)
 
     adx = round(float(df["ADX"].iloc[-1]),2)
     rsi = round(float(df["RSI"].iloc[-1]),2) 
+    bb =df["BB_Upper"].iloc[-1] < df["KC_Upper"].iloc[-1] and \
+        df["BB_Lower"].iloc[-1] > df["KC_Lower"].iloc[-1]
 
     no_trend=adx <20
     rsi_flat =40 <=rsi <=60
     print(f"ADX:{adx} →{'There is no trend,stock is squeezed' if adx <20 else 'A trend exists'}")
     print(f"RSI: {rsi} → {'Squeeze band' if rsi_flat else 'Not squeezed'}")
+    print(f"BB inside Keltner Channel: {'Yes' if bb else 'no'}")
 
-    if no_trend and rsi_flat:
+    if no_trend and rsi_flat and bb:
         print("\n Squeeze is detected!")
     else:
         print("\n No squeeze.")
-    # --- Last month's squeeze history --- #0
+    # --- Last month's squeeze history --- 
     last_month = df[df.index >= df.index[-1] - pd.Timedelta(days=30)]
-    print(f"\n--- Last month's squeeze history ---") #0
-    squeeze_count = 0 #0
-    for date, row in last_month.iterrows(): #0
-        adx_d = round(float(row["ADX"]), 2) #0
-        rsi_d = round(float(row["RSI"]), 2) #0
-        is_squeeze = adx_d < 20 and 40 <= rsi_d <= 60 #0
-        if is_squeeze: #0
-            squeeze_count += 1 #0
-            print(f"{date.strftime('%d.%m.%Y')}: Squeeze  RSI: {rsi_d}  ADX: {adx_d}") #0
-    print(f"\nTotal {squeeze_count} days squeezed.") #0
+    print(f"\n--- Last month's squeeze history ---") 
+    squeeze_count = 0 
+    for date, row in last_month.iterrows(): 
+        adx_d = round(float(row["ADX"]), 2) 
+        rsi_d = round(float(row["RSI"]), 2) 
+        bb_d=row["BB_Upper"] < row["KC_Upper"] and row["BB_Lower"] > row ["KC_Lower"]
+        is_squeeze = adx_d < 20 and 40 <= rsi_d <= 60 and bb_d 
+        if is_squeeze: 
+            squeeze_count += 1 
+            print(f"{date.strftime('%d.%m.%Y')}: Squeeze  RSI: {rsi_d}  ADX: {adx_d}") 
+    print(f"\nTotal {squeeze_count} days squeezed.") 
